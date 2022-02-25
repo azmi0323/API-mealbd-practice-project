69398ddeb3ba74e08865f501e50908599be7c85d
AzmiHabib <azmihabib0323@gmail.com>
2022-02-25 11:14:19 +0600

    second push

--- js/mealbd.js
+++ js/mealbd.js
@@ -2,18 +2,24 @@ const searchFood = () => {
     const foodFeild = document.getElementById('food-feild');
     const foodFeildText = foodFeild.value;
     // console.log(foodFeildText)
+   if(foodFeild.value !=''){
     foodFeild.value = ''
+    
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodFeildText}`
 
     fetch(url)
         .then(res => res.json())
         .then(data => getValue(data.meals))
+        .catch(error => console.log(error))
+
+   }
 }
 
 const getValue = meals => {
     meals.forEach(meal => {
         // console.log(meal)
         const searchResult = document.getElementById('search-result');
+        
         const div = document.createElement('div')
         div.classList.add('col');
         div.innerHTML = `
@@ -25,7 +31,9 @@ const getValue = meals => {
             </div>
         </div>
         `;
+        
         searchResult.appendChild(div)
+        
     })
 }
 
@@ -40,18 +48,34 @@ const mealDetails = mealId => {
 
 const mealDocument = meal => {
     console.log(meal)
-    const mealDiscription = document.getElementById('meal-details');
+    const mealDiscription = document.getElementById('alart-box');
+    mealDiscription.classList.remove('d-none');
+    document.body.style.overflowY='hidden'
+    mealDiscription.textContent =''
     const div = document.createElement('div');
     div.classList.add('newDiv')
     div.innerHTML = `
-        <div class="card mb-3 mx-auto w-50">
-            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${meal.strMeal}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                content. This content is a little bit longer.</p>
+         <div class="card mb-3 mx-auto" style="max-width: 80%;">
+            <div class="row g-0 align-items-center">
+                <div class="col-md-4">
+                    <img src="${meal.strMealThumb}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <p class="card-text">${(meal.strInstructions).slice(0,900)}.</p>
+                    </div>
+                </div>
             </div>
         </div>
+        
     `
     mealDiscription.appendChild(div)
 }
+
+const detailList = document.getElementById('alart-box');
+detailList.addEventListener('click',function(){
+    detailList.classList.add('d-none')
+    document.body.style.overflowY='visible'
+
+})
